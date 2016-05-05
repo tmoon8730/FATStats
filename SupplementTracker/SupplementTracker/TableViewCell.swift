c6d093589b25188a420927c7008228c7474deebe
Tyler Moon(Home) <tmoon@Tylers-MacBook-Pro.local>
2016-05-05 19:04:56 -0400

    Got the right swipe to confirm done but no background highlighting

--- SupplementTracker/SupplementTracker/TableViewCell.swift
+++ SupplementTracker/SupplementTracker/TableViewCell.swift
@@ -20,6 +20,8 @@ class TableViewCell: UITableViewCell {
     let gradientLayer = CAGradientLayer()
     var originalCenter = CGPoint()
     var deleteOnDragRelease = false
+    var completeOnDragRelease = false
+    var itemCompleteLayer = CALayer()
     var delegate: TableViewCellDelegate?
     var supplementItem: NSManagedObject?
     
@@ -28,7 +30,10 @@ class TableViewCell: UITableViewCell {
     }
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        
         super.init(style:style, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .None
         
         gradientLayer.frame = bounds
         let color1 = UIColor(white: 1.0, alpha: 0.2).CGColor as CGColorRef
@@ -40,15 +45,23 @@ class TableViewCell: UITableViewCell {
         layer.insertSublayer(gradientLayer, atIndex: 0)
         
         
+        itemCompleteLayer = CALayer(layer:layer)
+        itemCompleteLayer.backgroundColor = UIColor(red:0.0, green:0.6, blue:0.0, alpha: 0.3).CGColor
+        itemCompleteLayer.hidden = true
+        layer.insertSublayer(itemCompleteLayer, atIndex: 0)
+        
         let recognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
         recognizer.delegate = self
         addGestureRecognizer(recognizer)
         
     }
+
+    let kLabelLeftMargin: CGFloat = 15.0
     override func layoutSubviews(){
         super.layoutSubviews()
         gradientLayer.frame = bounds
     }
+    
     
     func handlePan(recognizer: UIPanGestureRecognizer){
         if recognizer.state == .Began {
@@ -58,17 +71,20 @@ class TableViewCell: UITableViewCell {
             let translation = recognizer.translationInView(self)
             center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
             deleteOnDragRelease = frame.origin.x < -frame.size.width / 2.0
+            completeOnDragRelease = frame.origin.x > -frame.size.width / 2.0
         }
         
         if recognizer.state == .Ended {
             let originalFrame = CGRect(x:0, y:frame.origin.y, width: bounds.size.width, height: bounds.size.height)
-            if !deleteOnDragRelease {
-                UIView.animateWithDuration(0.2,animations: {self.frame = originalFrame})
-            }
             if deleteOnDragRelease{
-                if (delegate != nil && supplementItem != nil){
-                    delegate?.supplementItemDeleted(supplementItem!)
+                if delegate != nil && supplementItem != nil {
+                    delegate!.supplementItemDeleted(supplementItem!)
                 }
+            } else if completeOnDragRelease {
+                itemCompleteLayer.hidden = false
+                UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
+            } else {
+                UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
             }
         }
     }
